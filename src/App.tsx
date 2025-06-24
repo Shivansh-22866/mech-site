63b7ecbddae6f4dd962f702cbb0e484ef71ec4bc
Shivansh-22866 <shivanshp0418@gmail.com>
2025-06-24 19:29:31 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-7xl'>
+        PPTX
+      </div>
+      <Button>
+        CLICK
+      </Button>
+    </>
+  )
+}
+
+export default App
